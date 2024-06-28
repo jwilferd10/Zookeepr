e86f239eeb158d5a5e398d0f66d40af412c2c163
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-06-28 12:34:29 -0700

    Commented out unused paths

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 // Left off at 11.4.3
-const { animals } = require('./data/animals');
+// const { animals } = require('./data/animals');
+
 const express = require('express');
 const PORT = process.env.PORT || 3001
 const app = express();
-const fs = require('fs');
-const path = require('path');
+// const fs = require('fs');
+// const path = require('path');
 
 // parse incoming string or array data 
 app.use(express.urlencoded({ extended: true }));
