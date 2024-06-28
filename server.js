463c3688358d4564bd146e0c13444a273531e21d
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-06-28 11:17:37 -0700

    Created new htmlRoutes with an index.js file inside. Moved the HTML logic from server.js into this new index.js file

--- server.js
+++ server.js
@@ -14,18 +14,6 @@ app.use(express.json());
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/index.html'));
-});
-
-app.get('/animals', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/animals.html'));
-});
-
-app.get('/zookeepers', (req, res) => {
-    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
-});
-
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}`);
 });
