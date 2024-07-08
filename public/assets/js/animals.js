dd12152bf6aa6da27c6c5beb8537f8ae116d7209
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-07-08 10:06:13 -0700

    Added comments to public/assets/js/animals.js. Also added a helpful tldr comment at the top of the file

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -1,9 +1,16 @@
+// This file is responsible for the animals.html search
+// It will query the /api/animals and fetch appropriate responses
+// Automatically features all animals in the API, handles specified searches
+// Prints out the card onto the html
+
 const $animalForm = document.querySelector('#animals-form');
 const $displayArea = document.querySelector('#display-area');
 
+// The html template for the animals card
 const printResults = resultArr => {
   console.log(resultArr);
 
+  // Map through the array and collect specified parameters, pass it onto html template
   const animalHTML = resultArr.map(({ id, name, personalityTraits, species, diet }) => {
     return `
   <div class="col-12 col-md-5 mb-3">
@@ -19,26 +26,33 @@ const printResults = resultArr => {
     `;
   });
 
+  // Add the card onto the displayArea's inner html content
   $displayArea.innerHTML = animalHTML.join('');
 };
 
+// Accepts formData (an object) and processes the data contained within it
 const getAnimals = (formData = {}) => {
   let queryUrl = '/api/animals?';
 
+  // Collect the formData object's key/value pair and chains it to the end of queryUrl
   Object.entries(formData).forEach(([key, value]) => {
     queryUrl += `${key}=${value}&`;
   });
 
-  console.log(queryUrl);
+  // Testing to see the queryUrl structure
+  // console.log(queryUrl);
 
+  // Fetch the specified request
   fetch(queryUrl)
   .then(response => {
+    // Handle for any errors
     if (!response.ok) {
       return alert('Error: ' + response.statusText);
     }
     return response.json();
   })
   .then(animalData => {
+    // Prints the html template
     console.log(animalData);
     printResults(animalData);
   })
@@ -50,8 +64,10 @@ const handleGetAnimalsSubmit = event => {
   const dietRadioHTML = $animalForm.querySelectorAll('[name="diet"]');
   let diet;
 
+  // Loop through the radio options and search for selected option
   for (let i = 0; i < dietRadioHTML.length; i += 1) {
     if (dietRadioHTML[i].checked) {
+      // The value of the selected option will be packaged into the animals object
       diet = dietRadioHTML[i].value;
     }
   }
@@ -60,20 +76,26 @@ const handleGetAnimalsSubmit = event => {
     diet = '';
   }
 
+  // Personality traits can have multiple entries. These will be saved into this array
   const personalityTraitArr = [];
   const selectedTraits = $animalForm.querySelector('[name="personality"').selectedOptions;
 
+  // Push the value of the selected traits into the array
   for (let i = 0; i < selectedTraits.length; i += 1) {
+    // The selectedTraits' value is pushed into the array
     personalityTraitArr.push(selectedTraits[i].value);
   }
 
+  // Join the words together
   const personalityTraits = personalityTraitArr.join(',');
 
+  // The animal object contains the parameters users can search from
   const animalObject = { diet, personalityTraits };
 
+  // search the api for the animal object (will later invoke the processprint the results)
   getAnimals(animalObject);
 };
 
-$animalForm.addEventListener('submit', handleGetAnimalsSubmit);
-
 getAnimals();
+
+$animalForm.addEventListener('submit', handleGetAnimalsSubmit);
