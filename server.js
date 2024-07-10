432134d8aed2f359e8e6ef92d3e67d3e84723d9a
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-07-09 19:26:29 -0700

    Added new comments to explain server.js logic

--- server.js
+++ server.js
@@ -4,17 +4,20 @@ const express = require('express');
 const PORT = process.env.PORT || 3001
 const app = express();
 
-// parse incoming string or array data 
+// Middleware to parse incoming string or array data 
 app.use(express.urlencoded({ extended: true }));
 
-// parse incoming JSON data
+// Middleware to parse incoming JSON data
 app.use(express.json());
 
+// Route handlers
 app.use('/api', apiRoutes);
 app.use('/', htmlRoutes);
 
+// Serve static files
 app.use(express.static('public'));
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`API server now on port http://localhost:${PORT}`);
 });
