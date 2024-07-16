70d1142b341cb4cc331242268865ffe158cd8feb
Jonathan Wilferd <jwilferd10@yahoo.com>
2024-07-15 19:37:51 -0700

    Applied semantic html to the animals card template.

--- public/assets/js/animals.js
+++ public/assets/js/animals.js
@@ -13,16 +13,20 @@ const printResults = resultArr => {
   // Map through the array and collect specified parameters, pass it onto html template
   const animalHTML = resultArr.map(({ id, name, personalityTraits, species, diet }) => {
     return `
-  <div class="col-12 col-md-5 mb-3">
-    <div class="card p-3" data-id=${id}>
-      <h4 class="text-primary">${name}</h4>
-      <p>Species: ${species.substring(0, 1).toUpperCase() + species.substring(1)}<br/>
-      Diet: ${diet.substring(0, 1).toUpperCase() + diet.substring(1)}<br/>
-      Personality Traits: ${personalityTraits
-        .map(trait => `${trait.substring(0, 1).toUpperCase() + trait.substring(1)}`)
-        .join(', ')}</p>
-    </div>
-  </div>
+  <article class="col-12 col-md-5 mb-3">
+    <section class="card p-3" data-id=${id}>
+      <header>
+        <h4 class="text-primary">${name}</h4>
+      </header>
+      <p>
+        Species: ${species.substring(0, 1).toUpperCase() + species.substring(1)}<br/>
+        Diet: ${diet.substring(0, 1).toUpperCase() + diet.substring(1)}<br/>
+        Personality Traits: ${personalityTraits
+          .map(trait => `${trait.substring(0, 1).toUpperCase() + trait.substring(1)}`)
+          .join(', ')}
+      </p>
+    </section>
+  </article>
     `;
   });
 
